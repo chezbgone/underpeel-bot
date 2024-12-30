5c64bd76a1eaac2572b639c5be193d67be16553d
Jason Chen <chezbgone@gmail.com>
2024-12-30 17:42:07 -0600

    feat: remember robomoji transactions in db

--- cogs/robomoji.py
+++ cogs/robomoji.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import logging
 
 from discord import (
+    AllowedMentions,
     ForumChannel,
     HTTPException,
     Interaction,
@@ -16,13 +17,17 @@ from discord import (
 from discord.ext import commands
 
 from config import CONFIG
-from database.robomoji_db import get_emoji_info, register_emoji_use, toggle_emoji
+import database.robomoji_db as db
 
 LOG = logging.getLogger(__name__)
 
 ROBOMOJI_COOLDOWN = timedelta(seconds=30)
+management_check = app_commands.checks.has_any_role(
+    CONFIG['board_role_id'],
+    CONFIG['mod_role_id'],
+    CONFIG['dev_role_id'],
+)
 
-@app_commands.guild_only
 @app_commands.guilds(CONFIG['discord_server_id'])
 class RobomojiCog(commands.GroupCog, group_name='robomoji'):
     def __init__(self, bot: commands.Bot):
@@ -30,7 +35,8 @@ class RobomojiCog(commands.GroupCog, group_name='robomoji'):
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
-        assert(message.guild is not None)
+        if message.guild is None:
+            return
         if message.guild.id != CONFIG['discord_server_id']:
             return
 
@@ -49,7 +55,7 @@ class RobomojiCog(commands.GroupCog, group_name='robomoji'):
                 LOG.info('tried to robomoji in bad {channel=}')
 
         author_id = message.author.id
-        last_reacted, emojis = get_emoji_info(author_id)
+        last_reacted, emojis = db.get_emoji_info(author_id)
         if len(emojis) == 0:
             return
         if (
@@ -58,32 +64,87 @@ class RobomojiCog(commands.GroupCog, group_name='robomoji'):
         ):
             # too soon, don't add reactions
             return
-        register_emoji_use(author_id)
+        db.register_emoji_use(author_id)
         for emoji in emojis:
             try:
                 await message.add_reaction(emoji)
             except HTTPException as e:
-                if e.code == 10014:
-                    LOG.error(f"emoji '{emoji}' does not exist. removing from database.")
-                    toggle_emoji(author_id, emoji)
+                if e.code == 10014 or (e.code == 50035 and 'emoji_id' in e.text):
+                    LOG.error(
+                        f"emoji '{emoji}' for {message.author.id}({message.author.name}) "
+                        f"does not exist. removing from database.",
+                    )
+                    db.toggle_emoji('SYSTEM', author_id, emoji, 'bot could not find emoji')
+                
 
-    @app_commands.command()
-    @app_commands.checks.has_any_role(
-        CONFIG['board_role_id'],
-        CONFIG['mod_role_id'],
-        CONFIG['dev_role_id'],
-    )
+    @app_commands.command(name='toggle')
+    @management_check
     @app_commands.describe(
         member='member to add the robomoji to',
         emoji='emoji to become the reaction',
+        reason='reason for toggling'
     )
-    async def toggle_emoji(self, interaction: Interaction, member: Member, emoji: str):
-        res = toggle_emoji(member.id, emoji)
-        LOG.info(f'{interaction.user} {res} robomoji {emoji} for {member}')
-        await interaction.response.send_message(f'{res} robomoji {emoji} for {member}')
+    async def toggle_emoji(
+        self,
+        interaction: Interaction,
+        member: Member,
+        emoji: str,
+        reason: str,
+    ):
+        act = db.toggle_emoji(interaction.user.id, member.id, emoji, reason)
+        act_preposition = lambda action: 'to' if action == 'added' else 'from'
+        LOG.info(
+            f'{interaction.user} {act} robomoji {emoji} '
+            f'{act_preposition(act)} {member} (reason: {reason})'
+        )
+        await interaction.response.send_message(f'{act} robomoji {emoji} {act_preposition(act)} {member}')
+
+    @app_commands.command(name='list')
+    @management_check
+    @app_commands.describe(
+        member='member to describe emojis for',
+    )
+    async def list_emoji(self, interaction: Interaction, member: Member):
+        _, emojis = db.get_emoji_info(member.id)
+        if len(emojis) == 0:
+            await interaction.response.send_message(f'{member.name} has no robomojis')
+            return
+        await interaction.response.send_message(' '.join(emojis))
+
+    @app_commands.command(name='history')
+    @management_check
+    @app_commands.describe(
+        member='member to get emoji history for',
+        limit='how many transactions to display (default: 15)',
+    )
+    async def emoji_history(self, interaction: Interaction, member: Member, limit: int=15):
+        history = db.get_emoji_changes(member.id, limit)
+        act_preposition = lambda action: 'to' if action == "added" else 'from'
+        mention_staff = lambda staff: 'SYSTEM' if staff == 'SYSTEM' else f'<@{staff}>'
+        response = [
+            f'here are the most recent robomoji commands for {member.name}:',
+            *(
+                ' '.join((
+                    f'<t:{round(transaction.time.timestamp())}>:',
+                    mention_staff(transaction.staff),
+                    transaction.action,
+                    transaction.emoji,
+                    act_preposition(transaction.action),
+                    f'<@{transaction.chatter_id}>',
+                    f'(reason: {transaction.reason})'
+                ))
+                for transaction in history
+            )
+        ]
+        await interaction.response.send_message(
+            '\n'.join(response),
+            allowed_mentions=AllowedMentions.none()
+        )
 
     @toggle_emoji.error
-    async def toggle_emoji_error(self, interaction: Interaction, error: app_commands.AppCommandError):
+    @list_emoji.error
+    @emoji_history.error
+    async def permissions_error(self, interaction: Interaction, error: app_commands.AppCommandError):
         if isinstance(error, app_commands.MissingAnyRole):
             await interaction.response.send_message(
                 "You don't have the permission for this command.",
--- database/robomoji_db.py
+++ database/robomoji_db.py
@@ -1,16 +1,100 @@
+from decimal import Decimal
 import logging
+import re
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Literal
+from typing import Literal, Self
 
 from database import the_table
 
-_key = lambda user_id: { 'id': user_id, 'sk': 'robomoji' }
-
 LOG = logging.getLogger(__name__)
 
-def get_emoji_info(user_id: int) -> tuple[datetime | None, set[str]]:
+"""
+SCHEMA:
+=======
+userid, robomoji#update#{timestamp} : {
+    staff_id: int | Literal['SYSTEM']
+    emoji: int
+    added : bool
+    reason : str
+}
+
+userid, robomoji#info : {
+    emojis: list[binary]
+    last_reacted: optional[datetime]
+}
+"""
+
+def _make_sk(*layers: str) -> str:
+    _sk_prefix = 'robomoji'
+    _sk_separator = '#'
+    return _sk_separator.join((_sk_prefix, *layers))
+
+def _make_key(user_id: int, *layers: str):
+    return {
+        'id': user_id,
+        'sk': _make_sk(*layers)
+    }
+
+_update_prefix = 'update'
+
+@dataclass
+class RobomojiTransaction:
+    time: datetime
+    staff: int | Literal['SYSTEM']
+    chatter_id: int
+    action: Literal['added', 'removed']
+    emoji: str
+    reason: str
+
+    @classmethod
+    def parse(cls, item) -> Self:
+        assert('sk' in item)
+        match = re.fullmatch(f'robomoji#{_update_prefix}#(?P<timestamp>.*)', item['sk'])
+        assert(match is not None)
+        timestamp = datetime.fromisoformat(match['timestamp'])
+
+        assert('staff_id' in item)
+        if (staff_id := item['staff_id']) != 'SYSTEM':
+            assert(type(staff_id) is Decimal)
+            staff_id = int(staff_id)
+
+        assert('id' in item)
+        assert(type(item['id']) is Decimal)
+        chatter_id = int(item['id'])
+
+        assert('added' in item)
+        assert(type(item['added']) is bool)
+        action = 'added' if item['added'] else 'removed'
+
+        assert('emoji' in item)
+        emoji = bytes(item['emoji']).decode()
+
+        assert('reason' in item)
+        reason = item['reason']
+
+        return cls(timestamp, staff_id, chatter_id, action, emoji, reason)
+
+def get_emoji_changes(user_id: int, limit=15) -> list[RobomojiTransaction]:
+    response = the_table().query(
+        KeyConditionExpression='id = :id AND begins_with(sk, :sk_prefix)',
+        ExpressionAttributeValues={
+            ':id': user_id,
+            ':sk_prefix': _make_sk(_update_prefix),
+        },
+        ScanIndexForward=False,
+        Limit=limit,
+    )
+    changes = response.get('Items', []) # type: ignore
+
+    return [
+        RobomojiTransaction.parse(item)
+        for item in changes
+    ]
+
+def get_emoji_info(user_id: int) -> tuple[datetime | None, list[str]]:
     response = the_table().get_item(
-        Key=_key(user_id),
+        Key=_make_key(user_id, 'info'),
         ProjectionExpression='emojis, last_reacted'
     )
     last_reacted_raw: str | None = response.get('Item', {}).get('last_reacted')  # type: ignore
@@ -20,34 +104,53 @@ def get_emoji_info(user_id: int) -> tuple[datetime | None, set[str]]:
     if last_reacted_raw is not None:
         last_reacted = datetime.fromisoformat(last_reacted_raw)
 
-    emojis = {bytes(emoji).decode() for emoji in emojis_raw}
+    emojis = [bytes(emoji).decode() for emoji in emojis_raw]
 
     return last_reacted, emojis
 
 def register_emoji_use(user_id: int):
     the_table().update_item(
-        Key=_key(user_id),
+        Key=_make_key(user_id, 'info'),
         UpdateExpression='SET last_reacted = :t',
         ExpressionAttributeValues={
             ':t': datetime.now().isoformat(),
         },
     )
 
-def toggle_emoji(user_id: int, emoji: str) -> Literal['added', 'removed']:
+def toggle_emoji(
+    staff_id: int | Literal['SYSTEM'],
+    user_id: int,
+    emoji: str,
+    reason: str,
+) -> Literal['added', 'removed']:
     _, user_emojis = get_emoji_info(user_id)
-    if emoji in user_emojis:
-        update_expression = 'DELETE emojis :emoji'
-        ret = 'removed'
-    else:
-        update_expression = 'ADD emojis :emoji'
-        ret = 'added'
+    operation = 'removed' if emoji in user_emojis else 'added'
 
-    the_table().update_item(
-        Key=_key(user_id),
-        UpdateExpression=update_expression,
-        ExpressionAttributeValues={
-            # unicode string emojis get messed up in transit
-            ':emoji': {emoji.encode()},
-        },
+    the_table().put_item(
+        Item={
+            **_make_key(user_id, _update_prefix, datetime.now().isoformat()),
+            'staff_id': staff_id,
+            'emoji': emoji.encode(),
+            'added': operation == 'added',
+            'reason': reason
+        }
     )
-    return ret
+    if operation == 'removed':
+        i = user_emojis.index(emoji)
+        # can't factor .update_item out because
+        # ExpressionAttributeValues can't be empty dict
+        the_table().update_item(
+            Key=_make_key(user_id, 'info'),
+            UpdateExpression=f"REMOVE emojis[{i}]"
+        )
+    else:
+        the_table().update_item(
+            Key=_make_key(user_id, 'info'),
+            UpdateExpression="SET emojis = list_append(if_not_exists(emojis, :empty), :emoji)",
+            ExpressionAttributeValues= {
+                # unicode string emojis get messed up in transit
+                ':emoji': [emoji.encode()],
+                ':empty': [],
+            }
+        )
+    return operation
